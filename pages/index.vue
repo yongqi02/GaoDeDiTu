2772ebe91bd828c4e09bd5f5774cf70c2b450f7f
heyq <yq9932@qq.com>
2023-07-28 21:15:14 +0800

    feat: initialize the project

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<el-container>
+		<el-header class="bg-gray-400 h-16">
+		</el-header>
+		<el-container>
+			<el-aside width="200px">Aside</el-aside>
+			<el-container>
+				<el-main>Main</el-main>
+				<el-footer>Footer</el-footer>
+			</el-container>
+		</el-container>
+	</el-container>
+</template>
+
+<style scoped>
+
+</style>
